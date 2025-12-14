074fb14763735b532b42535fe2264b582f42c736
HAPPY <happy37135535@gmail.com>
2025-12-14 08:47:39 +0700

    Update EditingProperties.lua

--- EditingProperties.lua
+++ EditingProperties.lua
@@ -1,4 +1,5 @@
-local UI = loadstring(game:HttpGet("https://raw.githubusercontent.com/HAPPY-script/HAPPY_admin_chat/refs/heads/main/UIRegistry.lua"))()
+local UI = shared.__UI_REGISTRY__
+if not UI then return warn("UIRegistry not ready") end
 
 local function set(name, prop, value)
     local obj = UI[name]
@@ -8,7 +9,33 @@ local function set(name, prop, value)
 end
 
 set("Version", "TextTransparency", 0)
+set("Version", "TextStrokeTransparency", 0)
+
+set("Back", "TextTransparency", 0)
+set("Done", "TextTransparency", 0)
+
+set("Tip", "TextTransparency", 0)
 set("Tip", "TextStrokeTransparency", 0.5)
+set("Tip2", "TextTransparency", 0)
+
+set("System2", "TextTransparency", 0)
+
+set("Title", "TextTransparency", 0)
+
+set("Character2", "TextTransparency", 0)
+set("GameHub2", "TextTransparency", 0)
+
+set("Name11", "TextTransparency", 1)
+set("Name12", "TextTransparency", 1)
+set("Name13", "TextTransparency", 1)
+
+set("ReportTitle", "TextTransparency", 0)
+set("MaxText", "TextTransparency", 0)
+set("TextBox", "TextTransparency", 0)
 set("OkButton", "TextTransparency", 0)
+
 set("MyFeedback", "TextTransparency", 0)
 set("AdminFeedback", "TextTransparency", 0)
+
+set("MyTitle", "TextTransparency", 0)
+set("AdminTitle", "TextTransparency", 0)
