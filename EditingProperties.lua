346941c3556e29aa848c60db508bb9368dd27ebf
HAPPY <happy37135535@gmail.com>
2025-12-14 09:27:58 +0700

    Update EditingProperties.lua

--- EditingProperties.lua
+++ EditingProperties.lua
@@ -1,42 +1,46 @@
-local UI = loadstring(game:HttpGet("https://raw.githubusercontent.com/HAPPY-script/HAPPY_admin_chat/refs/heads/main/UIRegistry.lua"))()
--- hoặc lấy từ shared nếu đã load trước
-UI = shared.UI or UI
+-- EditingProperties.lua
+local UI = shared.UI
+if not UI or not UI.Get then
+    warn("UI Registry not ready")
+    return
+end
 
 local function safeSet(name, prop, value)
     local inst = UI.Get(name)
     if not inst then return false end
-    pcall(function() inst[prop] = value end)
-    return true
+    local ok = pcall(function()
+        inst[prop] = value
+    end)
+    return ok
 end
 
-set("Version", "TextTransparency", 0)
-set("Version", "TextStrokeTransparency", 0)
+safeSet("Version", "TextTransparency", 0)
+safeSet("Version", "TextStrokeTransparency", 0)
 
-set("Back", "TextTransparency", 0)
-set("Done", "TextTransparency", 0)
+safeSet("Back", "TextTransparency", 0)
+safeSet("Done", "TextTransparency", 0)
 
-set("Tip", "TextTransparency", 0)
-set("Tip", "TextStrokeTransparency", 0.5)
-set("Tip2", "TextTransparency", 0)
+safeSet("Tip", "TextTransparency", 0)
+safeSet("Tip", "TextStrokeTransparency", 0.5)
+safeSet("Tip2", "TextTransparency", 0)
 
-set("System2", "TextTransparency", 0)
+safeSet("System2", "TextTransparency", 0)
+safeSet("Title", "TextTransparency", 0)
 
-set("Title", "TextTransparency", 0)
+safeSet("Character2", "TextTransparency", 0)
+safeSet("GameHub2", "TextTransparency", 0)
 
-set("Character2", "TextTransparency", 0)
-set("GameHub2", "TextTransparency", 0)
+safeSet("Name11", "TextTransparency", 1)
+safeSet("Name12", "TextTransparency", 1)
+safeSet("Name13", "TextTransparency", 1)
 
-set("Name11", "TextTransparency", 1)
-set("Name12", "TextTransparency", 1)
-set("Name13", "TextTransparency", 1)
+safeSet("ReportTitle", "TextTransparency", 0)
+safeSet("MaxText", "TextTransparency", 0)
+safeSet("TextBox", "TextTransparency", 0)
+safeSet("OkButton", "TextTransparency", 0)
 
-set("ReportTitle", "TextTransparency", 0)
-set("MaxText", "TextTransparency", 0)
-set("TextBox", "TextTransparency", 0)
-set("OkButton", "TextTransparency", 0)
+safeSet("MyFeedback", "TextTransparency", 0)
+safeSet("AdminFeedback", "TextTransparency", 0)
 
-set("MyFeedback", "TextTransparency", 0)
-set("AdminFeedback", "TextTransparency", 0)
-
-set("MyTitle", "TextTransparency", 0)
-set("AdminTitle", "TextTransparency", 0)
+safeSet("MyTitle", "TextTransparency", 0)
+safeSet("AdminTitle", "TextTransparency", 0)
