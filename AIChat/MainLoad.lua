d13e0317539d0bb4340c1a349c920379881eb992
HAPPY <happy37135535@gmail.com>
2026-02-06 22:00:52 +0700

    Update MainLoad.lua

--- AIChat/MainLoad.lua
+++ AIChat/MainLoad.lua
@@ -1,1 +1,11 @@
+-- UI
+loadstring(game:HttpGet("https://raw.githubusercontent.com/HAPPY-script/HAPPY_admin_chat/refs/heads/main/AIChat/SupportReportGui_UI.lua"))()
+print("Done UI ✅")
 
+-- UI System
+loadstring(game:HttpGet("https://raw.githubusercontent.com/HAPPY-script/HAPPY_admin_chat/refs/heads/main/AIChat/UI_System.lua"))()
+print("Done UI System ✅")
+
+-- Chat System
+loadstring(game:HttpGet("https://raw.githubusercontent.com/HAPPY-script/HAPPY_admin_chat/refs/heads/main/AIChat/ChatSystem.lua"))()
+print("Done Chat System ✅")
